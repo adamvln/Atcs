3d0f17d676b1e5a9daab10479180fd65c6c5949a
Valin <adam.nilav@gmail.com>
2023-04-21 10:29:45 +0200

    eval and notebook

--- Practical1/eval.py
+++ Practical1/eval.py
@@ -4,6 +4,7 @@ import torch.optim as optim
 import os 
 import argparse
 import pickle
+import json
 
 from data import load_data, load_data_debug, Vocabulary, prepare_minibatch
 from torch.utils.data import DataLoader, Dataset
@@ -14,7 +15,7 @@ def evaluate(model, test_loader):
     criterion = nn.CrossEntropyLoss() # specify loss function
     model.eval() # set model to evaluation mode
     total_loss = 0
-    total_correct = 0
+    valid_preds = 0
     with torch.no_grad():
         for (x_premise, x_hypo, y_label) in test_loader:
 
@@ -24,50 +25,46 @@ def evaluate(model, test_loader):
 
             total_loss += loss.item() 
 
-            pred = y_pred.argmax(dim=1, keepdim=True) # get index of predicted class
-
-            total_correct += pred.eq(y_label.view_as(pred)).sum().item() # accumulate correct predictions
+            _, predicted = torch.max(y_pred, 1)
+            valid_preds += (predicted == y_label).sum().item()
 
     avg_loss = total_loss / len(test_loader.dataset) # calculate average loss
-    accuracy = total_correct / len(test_loader.dataset) # calculate accuracy
+    accuracy = valid_preds / len(test_loader.dataset) # calculate accuracy
     print('Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
-        avg_loss, total_correct, len(test_loader.dataset),
+        avg_loss, valid_preds, len(test_loader.dataset),
         100. * accuracy))
     return accuracy
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--encoder', default = 'average', type = str, help = "encoders to use")
+    parser.add_argument('--path_to_model', type = str)
     # parser.add_argument('--model_name', default = 'test', type = str, help = "model name to save in files")
 
     args = parser.parse_args()
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    dataset = load_data_debug()
-    vocab = Vocabulary(dataset)
-    vocab.build() 
+    dataset = load_data()
+    # vocab = Vocabulary(dataset)
+    # vocab.build() 
 
-    file_name = 'Practical1\data\embedding_matrix.pickle'
+    print("Loading of Vocabulary")
+    with open('data/data.json', 'r') as infile:
+        vocab_w2i = json.load(infile)
+    print("Vocabulary loaded")
+
+    file_name = 'data/embedding_matrix.pickle'
     # Open the file in read-binary ('rb') mode
     with open(file_name, 'rb') as file:
         # Load the data from the file
         data = pickle.load(file)
-    
-    if args.encoder == 'Average':
-        encoder = Average_Encoder(len(data), 300, data, device)
-    elif args.encoder == 'UniLSTM':
-        encoder = Unidir_LSTM(len(data), 300, 2048, data, device)
-    elif args.encoder == 'BiLSTM':
-        encoder = Bidirect_LSTM(len(data), 300, 2048, data, device)
-    elif args.encoder == 'BiLSTMConcat':
-        encoder = Bidirect_LSTM_Max_Pooling(len(data), 300, 2048, data, device)
 
-    model = Classifier(encoder, 300, device)
+    model = torch.load(args.path_to_model)
 
     from functools import partial
-    my_collate_fn = partial(prepare_minibatch, vocab = vocab)
-    test_loader = DataLoader(dataset["test"], batch_size = 4, shuffle = True, collate_fn = my_collate_fn)
+    my_collate_fn = partial(prepare_minibatch, vocab = vocab_w2i)
+    test_loader = DataLoader(dataset["test"], batch_size = 64, shuffle = True, collate_fn = my_collate_fn)
 
     evaluate(model, test_loader)
 
